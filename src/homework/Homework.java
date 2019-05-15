a6f6b219512a202b312ea27703051e6c62009ad7
fecarretero <fecarretero@gmail.com>
2019-05-14 23:55:04 -0300

    Version mas simplificada basada solo en Strings.(Agregado de Metodos EscribirNumero y CompararNumero)

--- src/homework/Homework.java
+++ src/homework/Homework.java
@@ -1,6 +1,11 @@
 package homework;
 
+import java.util.Random;
+import java.util.regex.Pattern;
+
 import javax.swing.JOptionPane;
+
+
 
 public class Homework {
 
@@ -15,9 +20,36 @@ public class Homework {
             System.out.println("Ingrese un numero de 4 digitos");
             numeroA = JOptionPane.showInputDialog("\"Ingrese un numero de 4 digitos\"");
 
-        } while (numeroA.length() < 4 || numeroA.length() > 4);
-
+        } while (numeroA.length() < 4 || numeroA.length() > 4 || (numeroA.matches("[a-zA-Z]+"))); //Evita letras y numeros con mas digitos.
+numberGenerator();
+GeneraNumero();
     }
+    
+ 
+public static int GeneraNumero() {
+    int respuesta;
+  Random num= new Random();
+    respuesta=(num.nextInt(9999)+ 1000);
+    System.out.println(respuesta);
+return respuesta;
+}
+public static int[] numberGenerator() {
+    Random randy = new Random();
+    int[] randArray = {10,10,10,10};
+
+    for(int i=0;i<randArray.length;i++){
+        int temp = randy.nextInt(10);
+        System.out.println(temp);
+        while(temp == randArray[0] || temp == randArray[1] || temp == randArray[2] || temp == randArray[3]){
+            temp=randy.nextInt(9);
+            
+        }
+        randArray[i]=temp;      
+    }
+    return randArray;
+}
+
+
 
     public String Comparar(String resp, String adiv) {
         int bien = 0;
