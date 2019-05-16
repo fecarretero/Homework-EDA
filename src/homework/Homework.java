dfa80ad5dc0308f2561e130ddbf9ae9c055af507
fecarretero <fecarretero@gmail.com>
2019-05-15 22:43:36 -0300

    Version terminada hasta la parte 1 (OOP)

--- src/homework/Homework.java
+++ src/homework/Homework.java
@@ -1,78 +1,77 @@
 package homework;
-
 import java.util.Random;
-import java.util.regex.Pattern;
-
 import javax.swing.JOptionPane;
-
-
 
 public class Homework {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        Numero a = new Numero();
+        int controlador;
+        String Num_Referencia;
+        String Num_Respuesta;
+        Num_Respuesta = Numero.GeneraNumero(); // Metodo que genera numero en string y imprime en consola por prueba
+        do {
+            Num_Referencia = EscribirNumero(); //Metodo que pregunta el numero a adivinar
+           controlador=Numero.Comparar(Num_Referencia,Num_Respuesta);
+        } while (4!=(controlador) );
+        JOptionPane.showMessageDialog(null, "Felicidades! Adivinaste el numero");
+    }
+
+    
+
+    public static String EscribirNumero() {
 
         String numeroA;
 
         do {
-            System.out.println("Ingrese un numero de 4 digitos");
             numeroA = JOptionPane.showInputDialog("\"Ingrese un numero de 4 digitos\"");
 
         } while (numeroA.length() < 4 || numeroA.length() > 4 || (numeroA.matches("[a-zA-Z]+"))); //Evita letras y numeros con mas digitos.
-numberGenerator();
-GeneraNumero();
+        return numeroA;
+
     }
-    
- 
-public static int GeneraNumero() {
-    int respuesta;
-  Random num= new Random();
-    respuesta=(num.nextInt(9999)+ 1000);
-    System.out.println(respuesta);
-return respuesta;
-}
-public static int[] numberGenerator() {
-    Random randy = new Random();
-    int[] randArray = {10,10,10,10};
 
-    for(int i=0;i<randArray.length;i++){
-        int temp = randy.nextInt(10);
-        System.out.println(temp);
-        while(temp == randArray[0] || temp == randArray[1] || temp == randArray[2] || temp == randArray[3]){
-            temp=randy.nextInt(9);
-            
-        }
-        randArray[i]=temp;      
-    }
-    return randArray;
-}
+    public static class Numero {
 
-
-
-    public String Comparar(String resp, String adiv) {
+        public static int Comparar(String resp, String adiv) {
         int bien = 0;
         int regular = 0;
-        int[] arr1 = new int[10];
-        int[] arr2 = new int[10];
+        char[] arr1 = new char[3];//Se separan en arrays las cadenas de numeros para comparar posiciones.
+        char[] arr2 = new char[3];//
+        arr1 = resp.toCharArray();//
+        arr2 = adiv.toCharArray();//
         
-        for (int i = 0; i < resp.length(); i++) {
-            char c1 = resp.charAt(i);
-            char c2 = resp.charAt(i);
+            for (int i = 0; i < resp.length(); i++) {//For que imprime en chars en consola para testing.
+                System.out.println("arr2 " + arr2[i]);
+                System.out.println(resp.indexOf(arr2[i]));
+                if (arr1[i] == arr2[i]) {
+                    bien++;
+                } else if (resp.contains(String.valueOf(arr2[i]))) {//Se compara que el char se encuentre en el numero a adivinar
+                    regular++;
+                }
 
-            if (c1 == c2) {
-                bien++;
-            } else {
-                arr1[c1 - '0']++;
-                arr2[c2 - '0']++;
             }
-        }
-
-        for (int i = 0; i < 10; i++) {
-            regular += Math.min(arr1[i], arr2[i]);
-        }
-
-        return bien + "A" + regular + "B";
+            JOptionPane.showMessageDialog(null, bien + "Bien" + regular + "Regular");
+            return bien;
     }
-}
+        public static String GeneraNumero() {
+        int respuesta;
+        String respuesta_String;
+        Random num = new Random();
+        do {
+            respuesta = (num.nextInt(9999) + 999);
+            System.out.println(respuesta);
+            respuesta_String = Integer.toString(respuesta);
+        } while (respuesta_String.length() != 4);
+
+        System.out.println(respuesta_String);
+        return respuesta_String;
+    }
+        }
+    }
+    
+    
+
+            
+    
+
