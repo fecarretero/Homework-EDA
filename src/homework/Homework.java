7cb931d65ccaaa1730fa0c79de945f4d1ef05d71
fecarretero <fecarretero@gmail.com>
2019-05-16 01:09:17 -0300

    1.1 +Agregado funcion de jugador (Sin terminar) +Agregado algoritmo de luhn para verificar numeros repetidos

--- src/homework/Homework.java
+++ src/homework/Homework.java
@@ -1,4 +1,5 @@
 package homework;
+
 import java.util.Random;
 import javax.swing.JOptionPane;
 
@@ -6,18 +7,35 @@ public class Homework {
 
     public static void main(String[] args) {
         Numero a = new Numero();
+
         int controlador;
         String Num_Referencia;
         String Num_Respuesta;
-        Num_Respuesta = Numero.GeneraNumero(); // Metodo que genera numero en string y imprime en consola por prueba
-        do {
-            Num_Referencia = EscribirNumero(); //Metodo que pregunta el numero a adivinar
-           controlador=Numero.Comparar(Num_Referencia,Num_Respuesta);
-        } while (4!=(controlador) );
-        JOptionPane.showMessageDialog(null, "Felicidades! Adivinaste el numero");
-    }
 
-    
+        int seleccion = JOptionPane.showOptionDialog(null, "Elija como jugar: ", //Cuadro de opciones para elegir jugador
+                "Seleccion de jugador", JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null,
+                new Object[]{"Adivinar", "Elegir Numero"}, "opcion 1");
+
+        if (seleccion == 0) {
+            Num_Respuesta = Numero.GeneraNumero(); // Metodo que genera numero en string y imprime en consola por prueba
+            do {
+                Num_Referencia = EscribirNumero(); //Metodo que pregunta el numero a adivinar
+                controlador = Numero.Comparar(Num_Referencia, Num_Respuesta, seleccion);
+            } while (4 != (controlador));
+            JOptionPane.showMessageDialog(null, "Felicidades! Adivinaste el numero");
+
+        } else {
+
+            Num_Respuesta = EscribirNumero();
+            do {
+                Num_Referencia = Numero.GeneraNumero();
+                controlador = Numero.Comparar(Num_Referencia, Num_Respuesta, seleccion);
+            } while (4 != (controlador));
+            JOptionPane.showMessageDialog(null, "La computadora adivino tu numero");
+        }
+
+    }
 
     public static String EscribirNumero() {
 
@@ -33,45 +51,59 @@ public class Homework {
 
     public static class Numero {
 
-        public static int Comparar(String resp, String adiv) {
-        int bien = 0;
-        int regular = 0;
-        char[] arr1 = new char[3];//Se separan en arrays las cadenas de numeros para comparar posiciones.
-        char[] arr2 = new char[3];//
-        arr1 = resp.toCharArray();//
-        arr2 = adiv.toCharArray();//
-        
-            for (int i = 0; i < resp.length(); i++) {//For que imprime en chars en consola para testing.
-                System.out.println("arr2 " + arr2[i]);
-                System.out.println(resp.indexOf(arr2[i]));
-                if (arr1[i] == arr2[i]) {
-                    bien++;
-                } else if (resp.contains(String.valueOf(arr2[i]))) {//Se compara que el char se encuentre en el numero a adivinar
-                    regular++;
+        public static int Comparar(String resp, String adiv, int juga) {
+            int bien = 0;
+            int regular = 0;
+            int jugador = juga;
+
+            char[] arr1 = resp.toCharArray();//Se separan en arrays las cadenas de numeros para comparar posiciones.
+            char[] arr2 = adiv.toCharArray();//
+            if (juga == 0) {
+                for (int i = 0; i < resp.length(); i++) {//For que imprime en chars en consola para testing.
+                    System.out.println("arr2 " + arr2[i]);
+                    System.out.println(resp.indexOf(arr2[i]));
+                    if (arr1[i] == arr2[i]) {
+                        bien++;
+                    } else if (resp.contains(String.valueOf(arr2[i]))) {//Se compara que el char se encuentre en el numero a adivinar
+                        regular++;
+                    }
                 }
 
             }
-            JOptionPane.showMessageDialog(null, bien + "Bien" + regular + "Regular");
+            JOptionPane.showMessageDialog(null, bien + " Bien " + regular + " Regular");
             return bien;
-    }
-        public static String GeneraNumero() {
-        int respuesta;
-        String respuesta_String;
-        Random num = new Random();
-        do {
-            respuesta = (num.nextInt(9999) + 999);
-            System.out.println(respuesta);
-            respuesta_String = Integer.toString(respuesta);
-        } while (respuesta_String.length() != 4);
-
-        System.out.println(respuesta_String);
-        return respuesta_String;
-    }
         }
-    }
-    
-    
+
+        public static String GeneraNumero() {
+            int respuesta;
+boolean controlador;
+            String respuesta_String;
+            Random num = new Random();
+
+            do {
+                do{
+                respuesta = (num.nextInt(9999) + 999);
+                respuesta_String = Integer.toString(respuesta);
+                   controlador= Duplicados(respuesta);
+                }while(controlador);
+            } while (respuesta_String.length() != 4);
+
+            System.out.println(respuesta_String);
+            return respuesta_String;
+        }
+
+        public static boolean Duplicados(int num){
+		boolean[] dup = new boolean[10];
+		while(num > 0){
+			if(dup[num%10]) return true;
+			dup[num%10] = true;
+			num/= 10;
+		}
+		return false;
 
             
-    
+        }
 
+    }
+
+}
